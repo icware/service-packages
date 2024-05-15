fbf7b4c272da57499f14d323bacb301c3f1eb420
TWtutoriais <magnusrevil748@gmail.com>
2024-05-15 19:29:28 -0300

    Alteração Checkbox
    imput checkbox

--- vue/routes/routes.js
+++ vue/routes/routes.js
@@ -1,12 +1,15 @@
-const vueRouts = [
+const prefix = "/test"; //Alterar prefixo
+
+//Definir rotas
+const Routers = [
   {
     path: "/test",
     name: "test",
 
     meta: {
-        unprotected:true,
-        title:'Rota de teste'
-    }
+      unprotected: true,
+      title: "Rota de teste",
+    },
 
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ViewVue.vue"),
@@ -17,12 +20,19 @@ const vueRouts = [
     name: "test",
 
     meta: {
-        unprotected:false,
-        title:'Rota de teste'
-    }
+      unprotected: false,
+      title: "Rota de teste",
+    },
 
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ViewVue.vue"),
   },
-
 ];
+
+//Alterar nome / exportar
+const vueRouts = Routers.map((route) => ({
+  ...route,
+  path: `${prefix}/${route.path}`,
+}));
+
+export default vueRouts;
